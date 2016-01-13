cae2b60833d7824b582d709772bae54c8d3dd63b
Brad Pizzimenti and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-13 10:38:28 -0800

    make looping branch for selection of animal links to blank animal pages

--- animals/js/scripts.js
+++ animals/js/scripts.js
@@ -1,10 +1,15 @@
 $(function() {
-    $('#lc-login').click(function() {
-      var animal = prompt("What animal do you want to learn about? Turtles, snakes, or insects?"));
+      var animal = prompt("What animal do you want to learn about? Turtles, snakes, or insects?");
 
-      if (animal === turtles) {
-        window.location.replace("turtles.html")
-      else if (animal ===
+
+      if (animal === "turtles") {
+        window.location.replace("turtles.html");
+      } else if (animal === "snakes"){
+        window.location.replace("snakes.html");
+      } else if (animal === "insects") {
+        window.location.replace("insects.html");
+      } else {
+        window.location.replace("index.html");
       }
 
 
@@ -12,13 +17,14 @@ $(function() {
 
 
 
-        var animal = $('#username').val();
-        var pass = $('#pass').val();
-
-        if (testUser(username) === true && testPass(pass) === true) {
-            window.location.replace("http://www.domain.com/home.html");
-        } else {
-            alert('warning message');
-        }
+    //
+    //
+    //     // var animal = $('#username').val();
+    //     // var pass = $('#pass').val();
+    //     //
+    //     // if (testUser(username) === true && testPass(pass) === true) {
+    //     //     window.location.replace("http://www.domain.com/home.html");
+    //     // } else {
+    //     //     alert('warning message');
+    //     // }
     });
-});
