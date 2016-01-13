f5f14394970914670e664a8fba131475958de523
Brad Pizzimenti and Ramon de la Cruz <brad.pizzimenti@gmail.com>
2016-01-13 11:48:12 -0800

    create loop for political candidate selection

--- politicalbeliefs/js/scripts.js
+++ politicalbeliefs/js/scripts.js
@@ -1,30 +1,12 @@
 $(function() {
-      var animal = prompt("What animal do you want to learn about? Turtles, snakes, or insects?");
+  $('#candidate form').submit(function(event) {
+    event.preventDefault();
+    var candidate = $("input#candidatename").val();
+    if (candidate === "trump") {
+    $('#trump').show();
+    } else if (candidate === "sanders") {
+    $('#sanders').show();
+    }
 
-
-      if (animal === "turtles") {
-        window.location.replace("turtles.html");
-      } else if (animal === "snakes"){
-        window.location.replace("snakes.html");
-      } else if (animal === "insects") {
-        window.location.replace("insects.html");
-      } else {
-        window.location.replace("index.html");
-      }
-
-
-
-
-
-
-    //
-    //
-    //     // var animal = $('#username').val();
-    //     // var pass = $('#pass').val();
-    //     //
-    //     // if (testUser(username) === true && testPass(pass) === true) {
-    //     //     window.location.replace("http://www.domain.com/home.html");
-    //     // } else {
-    //     //     alert('warning message');
-    //     // }
-    });
+  });
+});
